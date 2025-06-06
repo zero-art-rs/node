3eea7cd7119a51c90bd38ac7e09e06fee9e976c4
serhii023 <serhii023@gmail.com>
2025-06-06 15:46:20 +0300

    upload old benches

--- crates/art/src/helper_tools.rs
+++ crates/art/src/helper_tools.rs
@@ -1,113 +1,19 @@
-use aes_gcm::{
-    Aes256Gcm, Key, Nonce,
-    aead::{Aead, AeadCore, KeyInit, OsRng},
-};
 use ark_bn254::fr::Fr as ScalarField;
-use ark_ff::PrimeField;
+use ark_bn254::fr::Fr as ARTScalarField;
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Compress, Validate};
-use ark_std::iterable::Iterable;
 use ark_std::{One, UniformRand, Zero};
-use hkdf::Hkdf;
-use rand::Rng;
-use rand::seq::SliceRandom;
-use sha2::{Digest, Sha256, Sha512};
+use rand::thread_rng;
 
 // return random ScalarField element, which isn't zero or one
 pub fn random_non_neutral_scalar_field_element() -> ScalarField {
     let mut rng = rand::thread_rng();
+
     let mut k = ScalarField::zero();
-    while k.eq(&ScalarField::one()) || k.eq(&ScalarField::zero()) {
+    while k.is_one() || k.is_zero() {
         k = ScalarField::rand(&mut rng);
     }
 
     k
-}
-
-// compute hash, and convert to ScalarField
-pub fn sha512_from_bytes(bytes: &Vec<u8>) -> Vec<u8> {
-    let mut hasher = Sha512::new();
-    hasher.update(bytes);
-    hasher.finalize()[..].to_vec()
-}
-
-// Given a list of scalars (a0, a1, ..., an) compute coefficients of
-// polynomial (x + a0)(x + a1)...(x + an)
-pub fn compute_polynomial_coefficients(roots: &Vec<ScalarField>) -> Vec<ScalarField> {
-    let n = roots.len();
-
-    let mut coefs = vec![ScalarField::zero(); n + 1];
-    coefs[0] = ScalarField::one();
-    let mut current_degree = 0;
-    for value in roots {
-        coefs[current_degree + 1] = coefs[current_degree];
-        for i in (1..=current_degree).rev() {
-            coefs[i] = coefs[i - 1] + coefs[i] * value;
-        }
-        coefs[0] *= value;
-
-        current_degree += 1;
-    }
-
-    coefs
-}
-
-pub fn hkdf(ikm: &Vec<u8>, salt: Option<&[u8]>, info: &[u8]) -> Vec<u8> {
-    let hk = Hkdf::<Sha512>::new(salt, ikm);
-    let mut okm = [0u8; 42];
-    hk.expand(&info, &mut okm)
-        .expect("42 is a valid length for Sha512 to output");
-
-    okm.to_vec()
-}
-
-pub fn encrypt_aes(key_bytes: Vec<u8>, plaintext: String) -> Result<Vec<u8>, String> {
-    let mut hasher = Sha256::new();
-    hasher.update(key_bytes);
-    let result = hasher.finalize();
-
-    let key = Key::<Aes256Gcm>::from_slice(&result[..]);
-    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
-    let cipher = Aes256Gcm::new(key);
-    let ciphered_data = cipher.encrypt(&nonce, plaintext.as_bytes());
-
-    let ciphered_data = match ciphered_data {
-        Ok(ciphered_data) => ciphered_data,
-        Err(e) => return Err(String::from(format!("Failed to encrypt: {:?}", e))),
-    };
-
-    let mut encrypted_data: Vec<u8> = nonce.to_vec();
-    encrypted_data.extend_from_slice(&ciphered_data);
-
-    Ok(encrypted_data)
-}
-
-pub fn decrypt_aes(key_bytes: Vec<u8>, encrypted_data: Vec<u8>) -> Result<String, String> {
-    let mut hasher = Sha256::new();
-    hasher.update(key_bytes);
-    let result = hasher.finalize();
-
-    let key = Key::<Aes256Gcm>::from_slice(&result[..]);
-    let (nonce_arr, ciphered_data) = encrypted_data.split_at(12);
-    let nonce = Nonce::from_slice(nonce_arr);
-    let cipher = Aes256Gcm::new(key);
-    let plaintext = cipher.decrypt(nonce, ciphered_data);
-
-    let plaintext = match plaintext {
-        Ok(plaintext) => String::from_utf8(plaintext),
-        Err(e) => return Err(String::from(format!("Failed to decrypt: {:?}", e))),
-    };
-
-    let plaintext = match plaintext {
-        Ok(plaintext) => plaintext,
-        Err(e) => {
-            return Err(String::from(format!(
-                "Failed to convert to string: {:?}",
-                e
-            )));
-        }
-    };
-
-    Ok(plaintext)
 }
 
 // For art serialisation
@@ -129,4 +35,13 @@ where
     let s: Vec<u8> = serde::de::Deserialize::deserialize(data)?;
     let a = A::deserialize_with_mode(s.as_slice(), Compress::No, Validate::Yes);
     a.map_err(serde::de::Error::custom)
+}
+
+pub fn create_random_secrets(size: usize) -> Vec<ARTScalarField> {
+    let mut secrets = Vec::new();
+    for x in 0..size {
+        secrets.push(ARTScalarField::rand(&mut thread_rng()));
+    }
+
+    secrets
 }
