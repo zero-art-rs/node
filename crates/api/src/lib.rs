c16aaf047b2e5c53b7ceb0ffb5ed7e04c6de307d
danielost <danya.ostap@gmail.com>
2025-07-16 17:40:33 +0300

    Add a test for message sending

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -11,6 +11,10 @@ pub(crate) mod domains;
 mod errors;
 mod router;
 
+#[cfg(test)]
+#[cfg(feature = "integration-tests")]
+mod tests;
+
 pub use container::Container;
 pub use domains::centrifugo::service::CentrifugoService;
 pub use domains::messenger::service::MessengerService;
