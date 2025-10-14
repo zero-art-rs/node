5b89e3e89ca9ef7e60edff980ab8ee41201d76b7
serhii023 <serhii023@gmail.com>
2025-10-14 14:43:12 +0300

    update dependencies

--- crates/proof-verifier/src/lib.rs
+++ crates/proof-verifier/src/lib.rs
@@ -25,6 +25,7 @@ pub type ProofVerifierReceiver = mpsc::Receiver<ProofVerifierMessageWrapper>;
 #[derive(Debug)]
 pub struct ProofVerifier {
     listener: ProofVerifierReceiver,
+    
 }
 
 impl ProofVerifier {
